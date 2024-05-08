af0fa8961bc87f8ec3242eb4b2fb642b3aeb4567
Ralph Goellner <ralph.goellner@e-ntegration.de>
2024-05-08 15:45:08 +0200

    flow-monitor ui added

---
+++ content/flows/readme.md
@@ -0,0 +1,2 @@
+# Flows
+Flow engineering results in one or more graphs guiding the agent through its task(s).
---
+++ frontends/react/flow-monitor/flow-monitor-app/app/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import React from 'react';
+import ReactFlow from 'reactflow';
+ 
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+  { id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+  { id: '2', position: { x: 0, y: 100 }, data: { label: '2' } },
+];
+const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div style={{ width: '100vw', height: '100vh' }}>
+          <ReactFlow nodes={initialNodes} edges={initialEdges} />
+      </div>
+    </main>
+  );
+}
