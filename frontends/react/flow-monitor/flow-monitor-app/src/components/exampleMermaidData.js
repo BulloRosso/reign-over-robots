a83833d4b7003bf63fdda1188cec66fadaab8549
Ralph Goellner <ralph.goellner@e-ntegration.de>
2024-06-12 16:14:09 +0200

    Mermaid state status highlighting

--- frontends/react/flow-monitor/flow-monitor-app/src/components/exampleMermaidData.js
+++ frontends/react/flow-monitor/flow-monitor-app/src/components/exampleMermaidData.js
@@ -1,9 +1,15 @@
 export default `
 stateDiagram-v2
+    direction TB
+
+    classDef active fill:gold
+
     [*] --> Waiting
     Waiting --> [*]: Due date reached
     Waiting --> Negotiating
     Negotiating --> Waiting
     Negotiating --> Success
     Success --> [*]
+
+    class Negotiating active
     `;
