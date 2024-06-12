f4c60be43fd63fe9b94ee5f61debb94061e5bffb
Ralph Goellner <ralph.goellner@e-ntegration.de>
2024-06-12 16:09:50 +0200

    Sequence diagram numbering and linking

--- frontends/react/flow-monitor/flow-monitor-app/src/components/exampleMermaidInteractionData.js
+++ frontends/react/flow-monitor/flow-monitor-app/src/components/exampleMermaidInteractionData.js
@@ -1,5 +1,10 @@
 export default `
 sequenceDiagram
+    autonumber
+    participant Alice
+    participant John
+    link Alice: Display Status @ agent/alice
+    link John: Display Status @ agent/john
     Alice->>+John: Hello John, how are you?
     Alice->>+John: John, can you hear me?
     John-->>-Alice: Hi Alice, I can hear you!
