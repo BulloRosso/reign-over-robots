6c1b0a091ba40e5cb5da71fbf564df682851f8a3
Ralph Goellner <ralph.goellner@e-ntegration.de>
2024-06-14 16:04:04 +0200

    more states

--- frontends/react/flow-monitor/flow-monitor-app/src/components/exampleMermaidData.js
+++ frontends/react/flow-monitor/flow-monitor-app/src/components/exampleMermaidData.js
@@ -8,9 +8,12 @@ stateDiagram-v2
     Planning --> Waiting
     Planning -->[*]: No tasks created
     Waiting --> [*]: Due date reached
+    Waiting --> [*]: Canceled by human in the loop
     Waiting --> Negotiating
     Negotiating --> Waiting
-    Negotiating --> Success
+    Negotiating --> PartialSuccess
+    Negotiating --> Success: All tasks completed
+    PartialSuccess --> [*]: 1+ tasks could not be completed
     Success --> [*]
 
     class Negotiating active
